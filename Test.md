d5f9ea3c8598e314aec97c324d2294db20d89bde
ChrisRDavila <crossdavila@gmail.com>
2023-04-06 19:46:13 -0700

    WIP

--- Test.md
+++ Test.md
@@ -1,39 +1,22 @@
-## Describe: Roll()
+## Describe: Game():
 
 ## Test: 
 "Testing to see if we can get a random number from our function each time."
 ## Code: 
-function Turn() {
-    let roll = Math.floor(Math.random() * 6) + 1;
-    return roll;}
+Game();
 ## Expected Output: 
 random number between 1 through 6.
 
-Test: "If dice rolls a 1, true, and returns false if not 1."
+## Test: "If dice rolls a 1, true, and returns false if not 1."
 ## Code:
-if(roll === 1) {
-    return true;
-    }
-else {
-    return roll
-    }
+Game();
 ## Expected Output: 
 True for 1 or roll 2-6.
 
 ## Test: 
 if player rolls a 2 or greater will add to sum, if player rolls a 1, turn is zeroed out and no points are added to players score.
 ## Code: 
-let sum = 0;
-function Turn(){
-  let roll = Math.floor(Math.random() * 6) + 1;
-    if(roll !== 1) {
-      sum += roll;
-      }
-    else {
-      sum = 0;
-      }
-      return sum;
-    } 
+Game()
 ## Expected Output: 
 if player rolls a 1 at any point then sum of points will reset to 0, if player rolls number other than 0, roll adds to previous roll
 
--- js/scripts.js
+++ js/scripts.js
@@ -1,52 +1,72 @@
 //Business Logic
-const playerOneScore = 0;
-const playerTwoScore = 0;
-const currentPlayer = 1;
-
-
-function diceRoll(){
-  let roll = Math.floor(Math.random() * 6) + 1;
-   
-  if(roll !== 1) {
-    if(currentPlayer === 1)
-      score1 += roll;
-        if(score1 >= 100)
-          window.alert("winner winner chicken dinner");
-          reset();
-      }
-    else {
-      score2 += roll;
-      } if(score2 >= 100){
-         window.alert("winner winner chicken dinner");
-         reset();
-      }
-    else {
-    currentPlayer = (currentPlayer === 1) ? 2 : 1;
-    updateScore();
+function Player (playerName) {
+  this.totalScore = 0;
+  this.turnScore = 0;
+  this.playerName = playerName;
 }
-  function reset() {
-    score1 = 0;
-    score2 = 0;
-    currentPlayer = 1;
-    updateScore();
-  }
 
-  function updateScore() {
-    document.getElementById("score1").textContent = score1;
-    document.getElementById("score2").textContent = score2;
+Player.prototype.rollDice = function() {
+  let diceToss = Math.floor(Math.random() * 6) + 1;
+  if (diceToss === 1) {
+    this.turnScore = 0;
+  } else {
+    this.turnScore += diceToss;
   }
+}
+Player.prototype.namingPlayer = function () {
+  this.nameOne = $("input#p1input-name").value();
+  this.nameTwo = $("input#p2input-name").value();
+}
 
+Player.prototype.hold = function () {
+  this.totalScore += this.turnScore:
+  this.turnScore = 0;
+}
+Player.prototype.restart = function () {
+  this.totalScore = 0;
+  this.turnScore = 0;
+}
 // UI Logic
-document.getElementById("rollDice").addEventListener("click", diceRoll );}
-
-//function displayWinner(event) {
-
-//}
-
-//function handleClick(event){
-
-//}
-
-//window.addEventListener("load, function()"{
-
-//})
+window.onload(function) {
+  let newPlayer1 = new Player ();
+  let newPlayer2 =new Player ();
+  let newPlayerName = new Player ();
+  ("form#names").submit(function(event) {
+    event.preventDefault();
+    newPlayerName.namingPlayer();
+    ("payer-one-name").text(newPlayerName.nameOne);
+    ("player-two-name").text(newPlayerName.nameTwo);
+  });
+  ("playerOneDice").click(function() {
+    newPlayer1.rollDice();
+    ("p1-turnscore").text(newPlayer1.turnscore);
+  });
+  ("p1-hold").click(function() {
+    newPlayer1.hold();
+    newPlayer1.namingPlayer(); 
+    if (newPlayer1.totalScore >= 100) {
+      document.remove(".hidden#winner");
+      const par = document.createElement("p");
+      par.append(newPlayerName.name1);
+  } else {
+    ("#playerOneDice,#p1-hold ").prop("disabled", true);
+    ("playerTwoDice, p2-hold").prop("disabled"), false;
+  }
+  });
+  ("#playerTwoDice").click(function() {
+    newPlayer2.rollDice();
+    $("#p2-turnscore").text(newPlayer2.turnScore);
+  });
+  $("#p2-hold").click(function() {
+    newPlayer2.hold();
+    newPlayerName.namingPlayer();
+    if (newPlayer2.totalScore >= 100) {
+        document.remove(".hidden#winner");
+        const par = document.createElement("p");
+        par.append(newPlayerName.name2);
+    } else {
+      $("#playerTwoDice, #p2-hold").prop("disabled", true);
+      $("#playerOneDice, #p1-hold").prop("disabled", false);
+    }
+  });
+}
